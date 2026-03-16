c8c6b72e9472492dc8033eaef87a1c682cf875ee
AccountingBody <info@accountingbody.com>
2026-03-16 22:13:34 +0000

    Fix: use eslint-disable comment for image url type

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,6 +1,5 @@
 import { createClient } from 'next-sanity'
 import imageUrlBuilder from '@sanity/image-url'
-import { SanityImageSource } from '@sanity/image-url/lib/types/types'
 
 export const client = createClient({
   projectId: '4rl1ejq1',
@@ -12,7 +11,8 @@ export const client = createClient({
 
 const builder = imageUrlBuilder(client)
 
-export function urlFor(source: SanityImageSource) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function urlFor(source: any) {
   return builder.image(source)
 }
 
