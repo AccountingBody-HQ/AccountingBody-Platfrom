9a68480bf2de526285381c3a4083dd6ae9daf3e0
Acctnbody <acctn.body@gmail.com>
2026-03-29 09:55:14 +0000

    Add embedded Sanity Studio at /studio route

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,9 +1,7 @@
-'use client'
+"use client"
 
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../sanity.config'
-
-export const dynamic = 'force-dynamic'
+import { NextStudio } from "next-sanity/studio"
+import config from "@/sanity.config"
 
 export default function StudioPage() {
   return <NextStudio config={config} />
