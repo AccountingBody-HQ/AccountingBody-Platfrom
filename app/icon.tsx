dbe62043be11cda5b82af313c8b73c36e0bc50e0
henokgetachewagga-bit <henok.getachew.agga@gmail.com>
2026-05-07 23:17:50 +0000

    Fix: favicon circular white background with navy logo mark

--- app/icon.tsx
+++ app/icon.tsx
@@ -10,13 +10,12 @@ export default function Icon() {
         width: 32,
         height: 32,
         background: '#ffffff',
-        borderRadius: 8,
+        borderRadius: '50%',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
-        border: '1px solid #e2e8f0',
       }}>
-        <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <svg width="18" height="18" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <rect x="0" y="0" width="9" height="20" rx="2" fill="#1e3a7a"/>
           <rect x="11" y="0" width="9" height="9" rx="2" fill="#1e3a7a"/>
           <rect x="11" y="11" width="9" height="9" rx="2" fill="#1e3a7a"/>
