f0acc6d33698b3cca965a1039ca379994763b8ee
Acctnbody <acctn.body@gmail.com>
2026-05-22 07:01:17 +0000

    fix: increase spine AB PRESS bottom offset to 92pt to clear white bar

--- components/book/FullCoverTemplate.tsx
+++ components/book/FullCoverTemplate.tsx
@@ -171,7 +171,7 @@ export function FullCoverTemplate({ subtitle, bookType, edition, pageCount, desc
     },
     spinePublisherWrap: {
       position: "absolute",
-      bottom: 80,
+      bottom: 92,
       left: 0,
       right: 0,
       alignItems: "center",
