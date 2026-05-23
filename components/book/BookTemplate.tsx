e05b5b9ea3ba4d55e89cbf833c21acad8f74e301
Acctnbody <acctn.body@gmail.com>
2026-05-23 08:33:55 +0000

    fix: add minPresenceAhead=120 to heading View in renderBlocks - prevents in-article heading orphaning

--- components/book/BookTemplate.tsx
+++ components/book/BookTemplate.tsx
@@ -290,7 +290,7 @@ function renderBlocks(blocks: any[]): React.ReactNode {
         ? <Text style={s.h2}>{content}</Text>
         : <Text style={s.h3}>{content}</Text>
       out.push(
-        <View key={i} wrap={false}>
+        <View key={i} wrap={false} minPresenceAhead={120}>
           {headingEl}
           {nextContent ? <Text style={s.body}>{nextContent}</Text> : null}
         </View>
