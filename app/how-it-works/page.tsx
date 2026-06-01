6fe8b346496784b7f2a5ce291313228f45b7ab0a
jamesmanx20-maker <james.manx20@gmail.com>
2026-06-01 12:48:48 +0000

    EthioTax: Fix unused isEthioTax var in how-it-works page

--- app/how-it-works/page.tsx
+++ app/how-it-works/page.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react'
 
 export default function HowItWorksPage() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [isEthioTax, setIsEthioTax] = useState(false)
 
   useEffect(() => {
