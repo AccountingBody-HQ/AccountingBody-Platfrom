97cd6084c906ec9ccb11d1f076c2de1503260cda
AccountingBody <info@accountingbody.com>
2026-09-13 20:13:10 +0000

    Revert "feat(jobs,practice-questions): share one Pagination component sitewide"
    This reverts commit 5365d7f36532151a8e77bbc3e268fb5f4862420f.

--- app/jobs/listings/JobListingsClient.tsx
+++ app/jobs/listings/JobListingsClient.tsx
@@ -26,7 +26,7 @@ import {
   type SortBy,
   type ListingsUrlState,
 } from './urlState'
-import { Pagination } from '@/components/Pagination'
+import { Pagination } from './Pagination'
 
 interface DirectJobsResponse {
   jobs?: Job[]
@@ -919,14 +919,7 @@ export default function JobListingsClient({ isEthioTax }: { isEthioTax: boolean 
                       />
                     ))}
                   </div>
-                  <Pagination
-                    page={page}
-                    totalPages={totalPages}
-                    total={total}
-                    navLabel="Job results pages"
-                    itemLabel="job"
-                    onChange={handlePageChange}
-                  />
+                  <Pagination page={page} totalPages={totalPages} total={total} onChange={handlePageChange} />
                 </div>
               </div>
             )}
